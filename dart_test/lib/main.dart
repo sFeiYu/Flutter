6a8c51341ef32282b20e398a84f41df11991292e
qianfeiying <qianfeiying@yaoyouke.com>
2020-01-20 10:11:41 +0800

    11

--- dart_test/lib/main.dart
+++ dart_test/lib/main.dart
@@ -25,48 +25,48 @@ class MyApp extends StatelessWidget {
 }
 
 //Container,Text用法
-//class HomeContent extends StatelessWidget{
-//  @override
-//  Widget build(BuildContext context) {
-//    // TODO: implement build
-//    return Center(
-//      child: Container(
-//        child: Text(
-//          '这是一个文本',
-//          textAlign: TextAlign.center,
-//          style: TextStyle(
-//            fontSize: 20.0,
-//            color: Colors.red,
-//            fontWeight: FontWeight.w800,
-//            fontStyle: FontStyle.italic,
-//            decoration: TextDecoration.lineThrough,
-//            decorationColor: Colors.white,
-//            decorationStyle: TextDecorationStyle.dashed,
-//            letterSpacing: 4.0,
-//          ),
-//          overflow: TextOverflow.ellipsis,
-//          maxLines: 1,
-//        ),
-//        height: 300.0,
-//        width: 300.0,
-//        decoration: BoxDecoration(
-//          color: Colors.blue,
-//          border: Border.all(
-//            color: Colors.orange,
-//            width: 2.0
-//          ),
-//          borderRadius: BorderRadius.all(
-//            //圆角
-//            Radius.circular(20.0),
-//          ),
-//        ),
-//        padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-////        transform: Matrix4.translationValues(0, 30, 0),
-//        transform: Matrix4.rotationZ(0.3),
-//        alignment: Alignment.center,
-//      ),
-//    );
-//  }
-//}
+class HomeContent extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Container(
+        child: Text(
+          '这是一个文本',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.red,
+            fontWeight: FontWeight.w800,
+            fontStyle: FontStyle.italic,
+            decoration: TextDecoration.lineThrough,
+            decorationColor: Colors.white,
+            decorationStyle: TextDecorationStyle.dashed,
+            letterSpacing: 4.0,
+          ),
+          overflow: TextOverflow.ellipsis,
+          maxLines: 1,
+        ),
+        height: 300.0,
+        width: 300.0,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          border: Border.all(
+            color: Colors.orange,
+            width: 2.0
+          ),
+          borderRadius: BorderRadius.all(
+            //圆角
+            Radius.circular(20.0),
+          ),
+        ),
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+//        transform: Matrix4.translationValues(0, 30, 0),
+        transform: Matrix4.rotationZ(0.3),
+        alignment: Alignment.center,
+      ),
+    );
+  }
+}
 
 
